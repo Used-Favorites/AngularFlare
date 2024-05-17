e9f4f6438b88bf05199314152410afd5cbe8365f
Polabiel <bielgabrieloliveira77@gmail.com>
2024-05-17 19:39:12 -0300

    Update .gitignore and package.json
    This commit updates the .gitignore file to ignore the android/ and ios/ directories. It also adds new dependencies to the package.json file.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.angular.usedfavorites',
+  appName: 'angularflare',
+  webDir: 'dist/angularflare'
+};
+
+export default config;
