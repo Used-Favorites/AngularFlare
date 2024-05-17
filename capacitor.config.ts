69f3d100a43de5989d70547c0614491147f322eb
Polabiel <bielgabrieloliveira77@gmail.com>
2024-05-17 20:41:52 -0300

    nova atualização

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.angular.usedfavorites',
   appName: 'angularflare',
-  webDir: 'dist/angularflare'
+  webDir: 'dist/angularflare/browser',
 };
 
 export default config;
